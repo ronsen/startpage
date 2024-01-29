72088f08d65f680b1bd8f3383891f3c926fcadee
Ronsen <ronsen@startrek.or.id>
2024-01-29 15:48:03 +0700

    add: clock

---
+++ src/lib/components/clock.svelte
@@ -0,0 +1,54 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+
+	const days = [
+		"Sunday",
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday"
+	];
+
+	const months = [
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	];
+
+	let time = new Date();
+
+	$: day = days[time.getDay()];
+	$: date = time.getDate();
+	$: month = months[time.getMonth()];
+	$: year = time.getFullYear();
+
+	$: hours = time.getHours();
+	$: minutes = time.getMinutes();
+	$: seconds = time.getSeconds();
+
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+</script>
+
+<div class="flex justify-between gap-3 uppercase text-xs">
+	<div>{day}, {date} {month} {year}</div>
+	<div>{hours}:{minutes}:{seconds}</div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,62 +1,15 @@
 <script lang="ts">
+	import Clock from "$lib/components/clock.svelte";
+	import Search from "$lib/components/search.svelte";
     import Link from "$lib/components/link.svelte";
-
-	let q: HTMLInputElement;
-	let e: HTMLSelectElement;
-
-	function handleSubmit() {
-		const key: string  = q.value.replaceAll(' ', '+');
-		const engine: string = e.value;
-		let url: string = '';
-
-		if (engine == 'DUCKDUCKGO') {
-			url = `https://duckduckgo.com/?q=${key}`;
-		}
-
-		if (engine == 'GOOGLE') {
-			url = `https://www.google.com/search?q=${key}`;
-		}
-
-		if (engine == 'BING') {
-			url = `https://www.bing.com/search?q=${key}`;
-		}
-
-		if (engine == 'YAHOO') {
-			url = `https://search.yahoo.com/search?p=${key}`;
-		}
-
-		if (engine == 'BRAVE') {
-			url = `https://search.brave.com/search?q=${key}`;
-		}
-
-		if (engine == 'YANDEX') {
-			url = `https://yandex.com/search/?text=${key}`;
-		}
-
-		window.location.href = url;
-	}
 </script>
 
+<section class="mb-3">
+	<Clock />
+</section>
+
 <section class="mb-10">
-	<form method="get" on:submit|preventDefault={handleSubmit}>
-		<div class="grid grid-cols-7 gap-1">
-			<div class="col-span-4">
-				<!-- svelte-ignore a11y-autofocus -->
-				<input bind:this={q}  type="text" class="w-full p-2 bg-zinc-600 text-gray-100 rounded-md outline-none font-bold" required autofocus>
-			</div>
-			<div class="col-span-2">
-				<select bind:this={e} class="w-full p-2.5 bg-zinc-700 text-white rounded-md outline-none font-bold">
-					<option value="DUCKDUCKGO">DuckDuckGo</option>
-					<option value="GOOGLE">Google</option>
-					<option value="BING">Bing</option>
-					<option value="YAHOO">Yahoo!</option>
-					<option value="BRAVE">Brave</option>
-					<option value="Yandex">Yandex</option>
-				</select>
-			</div>
-			<button type="submit" class="w-full p-2 bg-blue-500 rounded-md text-white font-bold outline-none">Search</button>
-		</div>
-	</form>
+	<Search />
 </section>
 
 <section class="mb-10">
