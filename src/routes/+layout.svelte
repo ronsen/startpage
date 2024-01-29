8fcad1160ab70889c38f9e6fecf9b40e21bb1fcc
Ronsen <ronsen@startrek.or.id>
2024-01-29 10:02:07 +0700

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import "../app.css";
+</script>
+
+<svelte:head>
+	<title>Start Page</title>
+</svelte:head>
+
+<main class="container md:w-[700px] mx-auto my-24">
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<section class="mb-10">
+	<form action="https://duckduckgo.com/" method="get">
+		<div class="grid grid-cols-4 gap-3">
+			<div class="col-span-3">
+				<!-- svelte-ignore a11y-autofocus -->
+				<input type="text" name="q" class="w-full p-2 bg-zinc-600 text-gray-100 rounded-sm" autofocus>
+			</div>
+			<button type="submit" class="w-full p-2 bg-blue-500 rounded-sm">Search</button>
+		</div>
+	</form>
+</section>
+
+<section class="mb-10">
+	<h4 class="uppercase text-xs font-bold mb-3">Favorites Links:</h4>
+
+	<div class="grid grid-cols-3">
+		<ul>
+			<li><a href="https://www.youtube.com/">YouTube</a></li>
+			<li><a href="https://twitter.com/">Twitter</a></li>
+			<li><a href="https://www.instagram.com/">Instagram</a></li>
+			<li><a href="https://www.reddit.com/">Reddit</a></li>
+			<li><a href="https://news.ycombinator.com/">Hacker News</a></li>
+		</ul>
+		<ul>
+			<li><a href="https://www.vidio.com/">Vidio</a></li>
+			<li><a href="https://www.twitch.tv/">Twitch</a></li>
+			<li><a href="https://www.tiktok.com/">TikTok</a></li>
+			<li><a href="https://kick.com/">Kick</a></li>
+			<li><a href="https://www.viddsee.com/">Viddsee</a></li>
+		</ul>
+		<ul>
+			<li><a href="https://github.com/">Github</a></li>
+			<li><a href="https://www.inoreader.com/">Inoreader</a></li>
+			<li><a href="https://feedly.com/">Feedly</a></li>
+			<li><a href="https://translate.google.com/">Google Translate</a></li>
+			<li><a href="https://chat.openai.com/">ChatGPT</a></li>
+		</ul>
+	</div>
+</section>
+
+<section class="mb-10">
+	<h4 class="uppercase text-xs font-bold mb-3">Other Links:</h4>
+
+	<ul>
+		<li><a href="https://aniwave.to/trending">Aniwave</a></li>
+		<li><a href="https://gitlab.com/">Gitlab</a></li>
+		<li><a href="https://psarips.com/">PSArips</a></li>
+		<li><a href="https://pahe.me/">Pahe Movies</a></li>
+	</ul>
+</section>
