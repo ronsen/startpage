f14dc8407490046eeb9e5f2911165e6834aa4359
Ronsen <ronsen@startrek.or.id>
2024-01-29 10:13:56 +0700

    update: link hover color to white

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,11 @@
 <section class="mb-10">
 	<form action="https://duckduckgo.com/" method="get">
-		<div class="grid grid-cols-4 gap-3">
-			<div class="col-span-3">
+		<div class="grid grid-cols-5 gap-3">
+			<div class="col-span-4">
 				<!-- svelte-ignore a11y-autofocus -->
-				<input type="text" name="q" class="w-full p-2 bg-zinc-600 text-gray-100 rounded-sm" autofocus>
+				<input type="text" name="q" class="w-full p-2 bg-zinc-600 text-gray-100 rounded-sm focus:outline-none" autofocus>
 			</div>
-			<button type="submit" class="w-full p-2 bg-blue-500 rounded-sm">Search</button>
+			<button type="submit" class="w-full p-2 bg-blue-500 rounded-sm text-white font-bold">Search</button>
 		</div>
 	</form>
 </section>
@@ -15,25 +15,25 @@
 
 	<div class="grid grid-cols-3">
 		<ul>
-			<li><a href="https://www.youtube.com/">YouTube</a></li>
-			<li><a href="https://twitter.com/">Twitter</a></li>
-			<li><a href="https://www.instagram.com/">Instagram</a></li>
-			<li><a href="https://www.reddit.com/">Reddit</a></li>
-			<li><a href="https://news.ycombinator.com/">Hacker News</a></li>
+			<li><a href="https://www.youtube.com/" class="hover:text-white">YouTube</a></li>
+			<li><a href="https://twitter.com/" class="hover:text-white">Twitter</a></li>
+			<li><a href="https://www.instagram.com/" class="hover:text-white">Instagram</a></li>
+			<li><a href="https://www.reddit.com/" class="hover:text-white">Reddit</a></li>
+			<li><a href="https://news.ycombinator.com/" class="hover:text-white">Hacker News</a></li>
 		</ul>
 		<ul>
-			<li><a href="https://www.vidio.com/">Vidio</a></li>
-			<li><a href="https://www.twitch.tv/">Twitch</a></li>
-			<li><a href="https://www.tiktok.com/">TikTok</a></li>
-			<li><a href="https://kick.com/">Kick</a></li>
-			<li><a href="https://www.viddsee.com/">Viddsee</a></li>
+			<li><a href="https://www.vidio.com/" class="hover:text-white">Vidio</a></li>
+			<li><a href="https://www.twitch.tv/" class="hover:text-white">Twitch</a></li>
+			<li><a href="https://www.tiktok.com/" class="hover:text-white">TikTok</a></li>
+			<li><a href="https://kick.com/" class="hover:text-white">Kick</a></li>
+			<li><a href="https://www.viddsee.com/" class="hover:text-white">Viddsee</a></li>
 		</ul>
 		<ul>
-			<li><a href="https://github.com/">Github</a></li>
-			<li><a href="https://www.inoreader.com/">Inoreader</a></li>
-			<li><a href="https://feedly.com/">Feedly</a></li>
-			<li><a href="https://translate.google.com/">Google Translate</a></li>
-			<li><a href="https://chat.openai.com/">ChatGPT</a></li>
+			<li><a href="https://github.com/" class="hover:text-white">Github</a></li>
+			<li><a href="https://www.inoreader.com/" class="hover:text-white">Inoreader</a></li>
+			<li><a href="https://feedly.com/" class="hover:text-white">Feedly</a></li>
+			<li><a href="https://translate.google.com/" class="hover:text-white">Google Translate</a></li>
+			<li><a href="https://chat.openai.com/" class="hover:text-white">ChatGPT</a></li>
 		</ul>
 	</div>
 </section>
@@ -42,9 +42,9 @@
 	<h4 class="uppercase text-xs font-bold mb-3">Other Links:</h4>
 
 	<ul>
-		<li><a href="https://aniwave.to/trending">Aniwave</a></li>
-		<li><a href="https://gitlab.com/">Gitlab</a></li>
-		<li><a href="https://psarips.com/">PSArips</a></li>
-		<li><a href="https://pahe.me/">Pahe Movies</a></li>
+		<li><a href="https://aniwave.to/trending" class="hover:text-white">Aniwave</a></li>
+		<li><a href="https://gitlab.com/" class="hover:text-white">Gitlab</a></li>
+		<li><a href="https://psarips.com/" class="hover:text-white">PSArips</a></li>
+		<li><a href="https://pahe.me/" class="hover:text-white">Pahe Movies</a></li>
 	</ul>
 </section>
