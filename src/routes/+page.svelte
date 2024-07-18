3c74dc938935b7fd0a5027e5560a7d844f0eaa8d
Ronsen <ronsen@startrek.or.id>
2024-07-18 15:13:27 +0700

    add: bootstrapcss icons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,8 @@
 
 		if (s) {
 			links = JSON.parse(s);
+		} else {
+			localStorage.setItem('links', JSON.stringify(links));
 		}
 	}
 
@@ -56,20 +58,8 @@
 				<div class="flex justify-between items-center gap-3">
 					<Link url={link.url} name={link.name} />
 					<button
-						on:click|preventDefault={deleteLink(link.url)}
-						class="hover:text-white"
-						><svg
-							xmlns="http://www.w3.org/2000/svg"
-							width="16"
-							height="16"
-							fill="currentColor"
-							class="bi bi-x"
-							viewBox="0 0 16 16"
-						>
-							<path
-								d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"
-							/>
-						</svg></button
+						on:click|preventDefault={() => deleteLink(link.url)}
+						class="hover:text-white"><i class="bi bi-x"></i></button
 					>
 				</div>
 			{/each}
